0eff212eecb3881153ddaa7ded2daa6eb9a4c4ce
TofaelKabir <tofaelkabir@gmail.com>
2024-07-06 11:51:31 -0400

    BDD Framework

---
+++ src/test/java/stepDefinition/LogInStepDef.java
@@ -0,0 +1,48 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LogInStepDef {
+	@Given("User is on the CMS Enterprise Portal page URL {string}")
+	public void user_is_on_the_cms_enterprise_portal_page_url(String string) {
+	    
+	}
+
+	@Then("User should validate the Title and Logo of the Application")
+	public void user_should_validate_the_title_and_logo_of_the_application() {
+	    
+	}
+
+	@When("User enter UserId in UserId field as {string}")
+	public void user_enter_user_id_in_user_id_field_as(String string) {
+
+	}
+
+	@When("User enter Password in Password field as {string}")
+	public void user_enter_password_in_password_field_as(String string) {
+	    
+	}
+
+	@When("User click the check box of I agree to the Terms and Conditions")
+	public void user_click_the_check_box_of_i_agree_to_the_terms_and_conditions() {
+	   
+	}
+
+	@When("User click on Login Button")
+	public void user_click_on_login_button() {
+	    
+	}
+
+	@Then("User will move to multi factor authentication modal")
+	public void user_will_move_to_multi_factor_authentication_modal() {
+	    
+	}
+
+
+
+
+
+
+}
---
+++ src/test/java/stepDefinition/NewUserRegistrationStepDef.java
@@ -0,0 +1,40 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class NewUserRegistrationStepDef {
+
+	@When("User click on New User Registration Button")
+	public void user_click_on_new_user_registration_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User navigate to Step one of the enrollment process")
+	public void user_navigate_to_step_one_of_the_enrollment_process() {
+		
+	}
+
+	@When("User complete Step one process")
+	public void user_complete_step_one_process() {
+	   
+	}
+
+	@Then("User navigate to Step two of the enrollment process")
+	public void user_navigate_to_step_two_of_the_enrollment_process() {
+		
+	}
+
+	@When("User complete Step two process")
+	public void user_complete_step_two_process() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User navigate to Step three of the enrollment process")
+	public void user_navigate_to_step_three_of_the_enrollment_process() {
+		
+	}
+
+}
